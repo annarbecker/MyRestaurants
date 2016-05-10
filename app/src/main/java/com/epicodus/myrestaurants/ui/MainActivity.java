663124ff3a0f74fe55c224a704d37588b3151e67
Anna Becker <anna.r.becker@gmail.com>
2016-05-10 11:30:05 -0700

    Remove search icon from main activity menu

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -69,7 +69,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search, menu);
+//        inflater.inflate(R.menu.menu_search, menu);
         inflater.inflate(R.menu.main_menu, menu);
 
         return true;
